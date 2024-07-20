a0a2fcb2079b6afd6836fb5430a457ca7c993e52
chuck0620 <horvath.adam01@yahoo.com>
2024-07-20 17:43:34 +0200

    Added some extra Light Emitting items

--- Assets/Items/SOs/LightEmittingItemSO.cs
+++ Assets/Items/SOs/LightEmittingItemSO.cs
@@ -8,9 +8,6 @@ public class LightEmittingItemSO : ItemSO
     [SerializeField] public float LightRange;
     [SerializeField] public Color LightColor;
     [SerializeField] public bool ShadowsEnabled;
-    [SerializeField] public float LightFlickerIntensity = 0; //TODO if we have time
-    [SerializeField] public float LightFlickerSpeed = 0; //TODO if we have time
-    [SerializeField] public float LightFlickerRange = 0; //TODO if we have time
     [SerializeField] public float FuelConsumptionRate = 1;
     [SerializeField] public float FuelLevel = 100;
 }
